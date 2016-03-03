6aaec521d7371ced17865b45efbe4f99a3e64ded
Ellie Dobson <edobson@pivotal.io>
2016-03-03 16:39:49 +0000

    msg

--- spec/java_buildpack/framework/matlab-spec.rb
+++ spec/java_buildpack/framework/matlab-spec.rb
@@ -20,47 +20,4 @@ require 'java_buildpack/framework/matlab'
 
 describe JavaBuildpack::Framework::Matlab do
   include_context 'component_helper'
-
-  it 'does not detect when rebel-remote.xml is not present' do
-    expect(component.detect).to be_nil
-  end
-
-  it 'detects when rebel-remote.xml is present in the top-level directory',
-     app_fixture: 'framework_jrebel_app_simple' do
-    expect(component.detect).to eq("jrebel-agent=#{version}")
-  end
-
-  it 'detects when rebel-remote.xml is present in WEB-INF/classes',
-     app_fixture: 'framework_jrebel_app_war' do
-    expect(component.detect).to eq("jrebel-agent=#{version}")
-  end
-
-  it 'detects when rebel-remote.xml is present inside an embedded JAR',
-     app_fixture: 'framework_jrebel_app_war_with_jar' do
-    expect(component.detect).to eq("jrebel-agent=#{version}")
-  end
-
-  it 'downloads the JRebel JAR and the native agent',
-     app_fixture: 'framework_jrebel_app_simple',
-     cache_fixture: 'stub-jrebel-archive.zip' do
-
-    component.compile
-
-    expect(sandbox + 'lib/MCR_R2015b_glnxa64_installer.zip').to exist
-  end
-
-  it 'adds correct arguments to JAVA_OPTS',
-     app_fixture: 'framework_jrebel_app_simple',
-     cache_fixture: 'stub-jrebel-archive.zip' do
-
-    allow(component).to receive(:architecture).and_return('x86_64')
-
-    component.release
-
-    expect(java_opts).to include('-agentpath:$PWD/.java-buildpack/jrebel_agent/lib/libjrebel64.so')
-    expect(java_opts).to include('-Drebel.remoting_plugin=true')
-    expect(java_opts).to include('-Drebel.log=true')
-    expect(java_opts).to include('-Drebel.cloud.platform=cloudfoundry/java-buildpack')
-  end
-
 end
