53f9d7c40fe887fc9c8441ae40ab2441be908211
Ellie Dobson <edobson@pivotal.io>
2016-03-08 14:00:32 +0000

    nm

--- lib/java_buildpack/framework/matlab.rb
+++ lib/java_buildpack/framework/matlab.rb
@@ -44,8 +44,14 @@ module JavaBuildpack
         #expect(@droplet.sandbox + 'MCR_R2015b_glnxa64_installer').to exist
         FileUtils.cd(@droplet.sandbox)
         puts "installation logs"
-        ret=system './install -agreeToLicense yes &>/home/vcap/logs/MCRinstall.out  &>log.out'
-        puts ret
+        system 'ls &>log2.out'
+        File.open("log2.out", "r") do |f|
+          f.each_line do |line|
+            puts "ellie"
+            puts line
+          end
+        end
+        ret=system './install -agreeToLicense yes &>log.out'
         File.open("log.out", "r") do |f|
           f.each_line do |line|
             puts "ellie"
